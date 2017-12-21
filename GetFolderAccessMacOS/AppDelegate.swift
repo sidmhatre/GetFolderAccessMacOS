a7ee78b75884e0ac8cd48d06b38e65b2156090af
Siddhesh Mhatre <siddhesh@sixmod5.com>
2017-12-21 15:58:05 +0530

    Initial Commit

---
+++ GetFolderAccessMacOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GetFolderAccessMacOS
+//
+//  Created by Siddhesh Mhatre on 21/12/17.
+//  Copyright © 2017 Siddhesh Mhatre. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GetFolderAccessMacOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GetFolderAccessMacOS
+//
+//  Created by Siddhesh Mhatre on 21/12/17.
+//  Copyright © 2017 Siddhesh Mhatre. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
